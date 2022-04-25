8a122a4d72b255558e363910db78500d6dd44af2
Javier <Javier.LOPEZ-ALONSO@ibl.cnrs.fr>
2022-04-25 14:03:41 +0200

    Fixed imports

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setup(
     packages=find_packages(),
     url="https://github.com/jlopezalo/pyafmreader",
     install_requires = [
+        'Cython',
         'fasterzip @ git+https://github.com/TkTech/fasterzip.git@09e2cae7821f96f4ba8f6d4122e1045352a656b4#egg=fasterzip',
     ]
 )
